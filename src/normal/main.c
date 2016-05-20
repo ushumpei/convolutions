2af6e9caeb691a0c5196d52f420d8b0b6e63bcfc
shumpei.uzawa <shumpei.uzawa@casleyconsulting.co.jp>
2016-05-20 18:37:48 +0900

    Fix normal convolution program

--- src/normal/main.c
+++ src/normal/main.c
@@ -1,70 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int MAX_FILTER_SIZE = 10;
-int MAX_DATA_SIZE = 100;
+#define MAX_SIGNALS_SIZE 1000
+#define MAX_STRINGS 32
 
-void convolute(float *filter, int filter_size, int *input_data, int input_data_size, int *output_data);
+void convolute(double *filter, int filter_length, double *signals, int signal_length, double *products);
+void get_double_data(const char *filename, double *data, int *datasize);
 
 int main(void) {
-  int i, ret, input_data_size = 0, filter_size = 0;
+  const char *signal_file   = "../data/input.csv";
+  double *signals;
+  int signal_length;
 
-  FILE *input_file;
-  FILE *filter_file;
-  char *input_file_name   = "../data/input.csv";
-  char *filter_file_name  = "../data/filter.csv";
+  const char *filter_file  = "../data/filter.csv";
+  double *filter;
+  int filter_length;
 
-  float filter_data[MAX_FILTER_SIZE];
-  int input_data[MAX_DATA_SIZE];
-  int output_data[MAX_DATA_SIZE];
+  double *products;
+  int i;
 
-  // INPUT DATAの読み込み
-  input_file = fopen( input_file_name, "r");
-  if( input_file == NULL) {
-    printf("%sファイルが開けません\n", input_file_name);
-    return 1;
-  }
-  while( (ret = fscanf( input_file, "%d", input_data+input_data_size)) != EOF
-      && input_data_size < MAX_DATA_SIZE) input_data_size++;
-  fclose(input_file);
-
-  // FILTERの読み込み
-  filter_file = fopen( filter_file_name, "r");
-  if( filter_file == NULL) {
-    printf("%sファイルが開けません\n", filter_file_name);
-    return 1;
-  }
-  while( (ret = fscanf( filter_file, "%f", filter_data+filter_size)) != EOF
-      && filter_size < MAX_FILTER_SIZE) filter_size++;
-  fclose(filter_file);
+  // ファイルから読み込む
+  get_double_data(filter_file, filter,  &filter_length);
+  get_double_data(signal_file, signals, &signal_length);
 
   // 畳み込みの実行
-  convolute(filter_data, filter_size, input_data, input_data_size, output_data);
+  convolute(filter, filter_length, signals, signal_length, products);
 
-  // 表示
-  printf("\nFILTER:\n");
-  for(i = 0; i < filter_size; i++) {
-    printf("%f ", filter_data[i]);
+  // 出力する
+  printf("\nSIGNALS:\n");
+  for(i = 0; i < signal_length; i++) {
+    printf("%d:\t%f\n", i, signals[i]);
   }
-  printf("\nINPUT:\n");
-  for(i = 0; i < input_data_size; i++) {
-    printf("%d ", input_data[i]);
+  printf("\nFILTER:\n");
+  for(i = 0; i < signal_length; i++) {
+    printf("%d:\t%f\n", i, filter[i]);
   }
   printf("\nOUTPUT:\n");
-  for(i = 0; i < input_data_size - filter_size + 1; i++) {
-    printf("%d ", output_data[i]);
+  for(i = 0; i < signal_length - filter_length; i++) {
+    printf("%d:\t%f\n", i, products[i]);
   }
-  printf("\nEND:\n");
 }
 
-void convolute(float *filter, int filter_size, int *input_data, int input_data_size, int *output_data) {
+void convolute(double *filter, int filter_length, double *signals, int signal_length, double *products) {
   int i, j;
   float tmp;
-  // loop from 0 to (DATA_SIZE - FILTER_SIZE + 1)
-  for(i = 0; i < input_data_size - filter_size + 1; i++) {
+  for(i = 0; i < signal_length - filter_length + 1; i++) {
     tmp = 0;
-    for(j = 0; j < filter_size; j++) {
-      tmp += *(filter+j) * *(input_data+i+j);
+    for(j = 0; j < filter_length; j++) {
+      tmp += filter[j] * signals[i+j];
     }
-    *(output_data+i) = tmp;
+    products[i] = tmp;
   }
+}
+
+void get_double_data(const char *filename, double *data, int *datasize) {
+  FILE *fp;
+  char line[MAX_STRINGS];
+
+  if ((fp = fopen(filename, "r")) == NULL) {
+    printf("ファイル「%s」のオープンに失敗しました。プログラムを終了します。\n", filename);
+    exit(1);
+  }
+
+  /* データ処理 */
+  *datasize = 0;
+  while (fgets(line, MAX_STRINGS, fp) != NULL) {
+    data[*datasize] = atof(line);
+    (*datasize)++;
+  }
+
+  /* ファイル・クローズ */
+  fclose(fp);
 }
